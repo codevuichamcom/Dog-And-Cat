a6cd2abe8869624585ab3506beef67abd7505e0e
lehongquan15a <lehongquan15a@gmail.com>
2020-02-22 18:07:16 +0700

    Let dog eat cat

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 var Dog = require('./Dog');
+var Cat = require('./Cat');
 
-var dog = new Dog("Tom");
-dog.sayHi();
+var dog = new Dog("Bruz");
+var tom = new Cat("Tom");
+dog.eat(tom);
+console.log(dog);
+
